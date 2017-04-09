06a0fa5b5a6fb80efd132f7d7f299bc7663e0b63
“heqinglin” <heqinglin@autohome.com.cn>
2017-04-09 14:45:30 +0800

    去掉Netutil工具类，会引起奔溃

--- app.main/src/main/java/com/qinglin/small/app/main/activity/MainActivity.java
+++ app.main/src/main/java/com/qinglin/small/app/main/activity/MainActivity.java
@@ -7,33 +7,35 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.qinglin.small.app.main.R;
-import com.qinglin.small.lib.business.utils.NetUtil;
+import com.qinglin.small.lib.business.utils.StringUtil;
 
 import net.wequick.small.Small;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private int count = 0;
+    private TextView tv1;
+    private TextView tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         checkNet();
-        TextView tv1 = (TextView) findViewById(R.id.tv1);
+        tv1= (TextView) findViewById(R.id.tv1);
         tv1.setOnClickListener(this);
-        TextView tv2 = (TextView) findViewById(R.id.tv2);
+        tv2 = (TextView) findViewById(R.id.tv2);
         tv2.setOnClickListener(this);
     }
 
     private void checkNet() {
-        if(!NetUtil.CheckNetState()){
-            Toast.makeText(this,"无网络",Toast.LENGTH_SHORT).show();
-        }else{
-            if(NetUtil.isWifi())
-                Toast.makeText(this,"WIFI",Toast.LENGTH_SHORT).show();
-            if(NetUtil.isMobile())
-                Toast.makeText(this,"手机网络",Toast.LENGTH_SHORT).show();
-        }
+//        if(!NetUtil.CheckNetState()){
+//            Toast.makeText(this,"无网络",Toast.LENGTH_SHORT).show();
+//        }else{
+//            if(NetUtil.isWifi())
+//                Toast.makeText(this,"WIFI",Toast.LENGTH_SHORT).show();
+//            if(NetUtil.isMobile())
+//                Toast.makeText(this,"手机网络",Toast.LENGTH_SHORT).show();
+//        }
     }
 
 
@@ -50,6 +52,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 lauchToCarPlgin();
                 break;
             case R.id.tv2:
+                Toast.makeText(MainActivity.this, StringUtil.trim(tv2.getText().toString()),Toast.LENGTH_SHORT).show();
                 break;
         }
     }
